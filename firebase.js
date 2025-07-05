89bfdaa86d00f96a503a5fc8812a5d96b01c7ef1
islam100 <70288544+islam100@users.noreply.github.com>
2025-07-05 19:59:18 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,9 @@
-
-// TODO: Replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCH17_8W_UPmPt4KqpCaV7BpN6KsA4a67E",
+  authDomain: "zara-fix.firebaseapp.com",
+  databaseURL: "https://zara-fix-default-rtdb.firebaseio.com",
+  projectId: "zara-fix",
+  storageBucket: "zara-fix.appspot.com", // ✅ सही यह है
+  messagingSenderId: "476473209200",
+  appId: "1:476473209200:web:9f6efe6c32fd4248949fb5"
 };
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
