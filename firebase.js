be884ba0a52be7e715d319588cb520ba6b4f78e9
islam100 <70288544+islam100@users.noreply.github.com>
2025-07-05 18:35:24 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+// TODO: Replace with your Firebase config
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
